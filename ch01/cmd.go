69c2df5e530ddd3d156689918d2252e56c1e6c90
ShaoRuichen <shao.ruichen@linecorp.com>
2021-06-03 10:59:06 +0800

    chapter 01

---
+++ ch01/cmd.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Cmd struct {
+	// 帮助位
+	helpFlag bool
+
+	// 版本位
+	versionFlag bool
+
+	// classpath
+	cpOption string
+	class    string
+	args     []string
+}
+
+func parseCmd() *Cmd {
+	cmd := Cmd{}
+
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "? ", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+
+	// 解析选项
+	flag.Parse()
+
+	// 捕获其他未被解析的参数
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+
+	return &cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
