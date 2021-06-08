7a5399d89b47a912e74c0b985e9846a62e77001b
ShaoRuichen <shao.ruichen@linecorp.com>
2021-06-08 14:36:35 +0800

    chapter 04

---
+++ rtda/thread.go
@@ -0,0 +1,24 @@
+package rtda
+
+type Thread struct {
+	pc    int
+	stack *Stack
+}
+
+func NewThread() *Thread {
+	return &Thread{
+		stack: newStack(1024),
+	}
+}
+
+func (t *Thread) PushFrame(frame *Frame) {
+	t.stack.push(frame)
+}
+
+func (t *Thread) PopFrame() *Frame {
+	return t.stack.pop()
+}
+
+func (t *Thread) CurrentFrame() *Frame {
+	return t.stack.top()
+}
